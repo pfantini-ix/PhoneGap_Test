ea4f118f2fb87c55a6936fe56f24a127f283c715
Pablo Fantini <pfantini@asanteit.com.ar>
2013-06-24 15:37:05 -0300

    add other qr

--- external/qr/decoder.js
+++ external/qr/decoder.js
@@ -1,95 +1,98 @@
-/*
-  Ported to JavaScript by Lazar Laszlo 2011 
-  
-  lazarsoft@gmail.com, www.lazarsoft.info
-  
-*/
-
-/*
-*
-* Copyright 2007 ZXing authors
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
-
-Decoder={};
-Decoder.rsDecoder = new ReedSolomonDecoder(GF256.QR_CODE_FIELD);
-
-Decoder.correctErrors=function( codewordBytes,  numDataCodewords)
-{
-	var numCodewords = codewordBytes.length;
-	// First read into an array of ints
-	var codewordsInts = new Array(numCodewords);
-	for (var i = 0; i < numCodewords; i++)
-	{
-		codewordsInts[i] = codewordBytes[i] & 0xFF;
-	}
-	var numECCodewords = codewordBytes.length - numDataCodewords;
-	try
-	{
-		Decoder.rsDecoder.decode(codewordsInts, numECCodewords);
-		//var corrector = new ReedSolomon(codewordsInts, numECCodewords);
-		//corrector.correct();
-	}
-	catch ( rse)
-	{
-		throw rse;
-	}
-	// Copy back into array of bytes -- only need to worry about the bytes that were data
-	// We don't care about errors in the error-correction codewords
-	for (var i = 0; i < numDataCodewords; i++)
-	{
-		codewordBytes[i] =  codewordsInts[i];
-	}
-}
-
-Decoder.decode=function(bits)
-{
-	var parser = new BitMatrixParser(bits);
-	var version = parser.readVersion();
-	var ecLevel = parser.readFormatInformation().ErrorCorrectionLevel;
-	
-	// Read codewords
-	var codewords = parser.readCodewords();
-
-	// Separate into data blocks
-	var dataBlocks = DataBlock.getDataBlocks(codewords, version, ecLevel);
-	
-	// Count total number of data bytes
-	var totalBytes = 0;
-	for (var i = 0; i < dataBlocks.Length; i++)
-	{
-		totalBytes += dataBlocks[i].NumDataCodewords;
-	}
-	var resultBytes = new Array(totalBytes);
-	var resultOffset = 0;
-	
-	// Error-correct and copy data blocks together into a stream of bytes
-	for (var j = 0; j < dataBlocks.length; j++)
-	{
-		var dataBlock = dataBlocks[j];
-		var codewordBytes = dataBlock.Codewords;
-		var numDataCodewords = dataBlock.NumDataCodewords;
-		Decoder.correctErrors(codewordBytes, numDataCodewords);
-		for (var i = 0; i < numDataCodewords; i++)
-		{
-			resultBytes[resultOffset++] = codewordBytes[i];
-		}
-	}
-	
-	// Decode the contents of that stream of bytes
-	var reader = new QRCodeDataBlockReader(resultBytes, version.VersionNumber, ecLevel.Bits);
-	return reader;
-	//return DecodedBitStreamParser.decode(resultBytes, version, ecLevel);
-}
+/**
+ * Loads qrcode dependencies, and creates a wrapper that only requires an <input type="file"...> as the handle
+ * 
+ * @example
+ * //<input id="codeimage" type="file">
+ * var callback = function(code){alert('the code was: ' + code);} 
+ * QRIfy('codeimg', callback);//will be connected on DOMContentLoaded
+ */
+(new function(world){
+    var head = document.getElementsByTagName('head')[0];
+    var dependencies = ["grid.js","version.js","detector.js","formatinf.js","errorlevel.js","bitmat.js","datablock.js","bmparser.js","datamask.js","rsdecoder.js","gf256poly.js","gf256.js","decoder.js","QRCode.js","findpat.js","alignpat.js","databr.js","mpimage.js"]
+    var scripts = document.getElementsByTagName('script');
+    var path = '';
+    for(i=0; i<scripts.length; i++){
+        if(scripts[i].src && scripts[i].src.indexOf('decoder.js') != -1){
+            path = scripts[i].src.replace('decoder.js','') + 'dependencies/';
+            break;
+        }
+    } 
+    
+    var loadScript = function(src){
+        document.write('<scr' + 'ipt type="text/javascript" src="' + src +'"></scr' + 'ipt>')
+    }
+    
+    for(var i=0; i<dependencies.length; i++){
+        loadScript(path + dependencies[i]);
+    }
+    
+    var qrField = function(elemOrId){
+        var elem = typeof(elemOrId) == 'string' ? document.getElementById(elemOrId) : elemOrId;
+        var self = this;
+        var spinner;
+        
+        self.callback = function(data){
+            console.log("Set callback to receive the data in the code, data was: " + data);
+        }
+        
+        
+        var onDecode = function(data){
+            self.callback(data && data.indexOf('error decoding') == -1 ? data : false);
+            document.body.removeChild(spinner);
+        }
+        
+        var handleFileSelect = function(evt){
+            //alert('changed')
+            var file = evt.target.files[0];
+            
+            spinner = document.createElement('img');
+            var orientation = window.innerWidth > window.innerHeight ? 'LANDSCAPE' : 'PORTRAIT';
+            spinner.setAttribute('style', (orientation == 'LANDSCAPE' ? 'height' : 'width') + ':50%; top:25%; position:absolute; left:25%;')
+            
+            var delayLoad = function(){
+                var img = new Image();
+                img.onload = function(){
+                   //document.getElementById('preview').src = this.src;
+                   //console.log('src is', this.src); 
+                   qrcode.callback = onDecode;
+                   qrcode.decode(this.src);
+                   
+                }
+                
+                var mpImg = new MegaPixImage(file);
+                mpImg.render(img, { maxWidth: 1000, maxHeight: 1000, quality: 1.0 });
+            }
+            
+            spinner.onload = function(){
+                setTimeout(delayLoad, 400)    
+            }
+            spinner.alt="Loading...";
+            spinner.innerHTML = "Loading...";
+            
+            spinner.src=path + 'spinner.gif';
+            document.body.appendChild(spinner);
+        }
+        
+        elem.addEventListener('DOMSubtreeModified', function(){alert('changed 1');})
+        elem.addEventListener('change', handleFileSelect, false);
+        
+            
+            
+        
+    }
+    
+    world.QRIfy = function(elemOrId, callback){
+        if(typeof(elemOrId) == 'string'){
+            var elem = document.getElementById(elemOrId);
+            if(!elem){
+                document.addEventListener('DOMContentLoaded', function(){QRIfy(elemOrId, callback);})
+                return;
+            }
+        }
+        var q = new qrField(elemOrId);
+        if(typeof callback == 'function'){
+            q.callback = callback;
+        }
+    }
+    
+}(window))
--- js/events/pagebeforeshow/qrJS.js
+++ js/events/pagebeforeshow/qrJS.js
@@ -1,32 +1,5 @@
 define(["qrScan"], function (qrscan) {
 	
-	var gCtx = null;
-	var gCanvas = null;
-	
-	var imageData = null;
-	var ii=0;
-	var jj=0;
-	var c=0;
-	
-	function initCanvas(ww,hh){
-		gCanvas = document.getElementById("qr-canvas");
-		var w = ww;
-		var h = hh;
-		gCanvas.style.width = w + "px";
-		gCanvas.style.height = h + "px";
-		gCanvas.width = w;
-		gCanvas.height = h;
-		gCtx = gCanvas.getContext("2d");
-		gCtx.clearRect(0, 0, w, h);
-		imageData = gCtx.getImageData( 0,0,320,240);
-	}
-	
-	function captureToCanvas() {
-		var flash = document.getElementById("embedflash");
-		flash.ccCapture();
-		qrcode.decode();
-	}
-	
 	return {
 		addEvent : function(){
 			$('#qrJS').unbind()
@@ -34,14 +7,18 @@ define(["qrScan"], function (qrscan) {
 	    		
 				console.log("load qr");
 				
-				initCanvas(640,480);
-				qrcode.callback = function(a){
-					console.log(a);
-				};
+				 var callback = function(code){alert('the code was: ' + code);} 
+				 QRIfy('codeimg', callback);//will be connected on DOMContentLoaded
 				
 				$('#btnQrJs').click(function(){	
-					console.log("click qr scan");
-					captureToCanvas();
+					
+					navigator.camera.getPicture( 
+						function(imageData) {
+						    var image = document.getElementById('myImage');
+    						image.src = "data:image/jpeg;base64," + imageData;
+						}, function(message) {
+						    alert("error camera")
+						});
 				});
 			});
 		},
